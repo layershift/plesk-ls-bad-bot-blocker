93ea16ebcdc9a7ef69293a38f18caa49dd28d6dd
Raul Adorean <raul@layershift.me>
2025-04-23 09:48:45 +0000

    Run sed only if a file is found

--- ls-bad-bot-blocker/sbin/undeploy.sh
+++ ls-bad-bot-blocker/sbin/undeploy.sh
@@ -10,8 +10,14 @@ if [ -f /etc/nginx/conf.d/botblocker-nginx-settings.conf ]; then
     rm -f /etc/nginx/conf.d/botblocker-nginx-settings.conf
 fi
 
-confFile="$(grep -r "\bserver_names_hash_bucket_size\b" /etc/nginx/nginx.conf /etc/nginx/conf.d/  | grep -vE "botblocker-nginx-settings.conf" | awk -F: '{print $1}' | head -n1)"
-sed 's/#server_names_hash_bucket_size/server_names_hash_bucket_size/g' -i $confFile
+while IFS= read -r confFile; do
+    if [ -f "$confFile" ]; then
+        sed 's/#server_names_hash_bucket_size/server_names_hash_bucket_size/g' -i $confFile
+    fi
+done <<<"$(grep -r "\bserver_names_hash_bucket_size\b" /etc/nginx/nginx.conf /etc/nginx/conf.d/  | grep -vE "botblocker-nginx-settings.conf" | awk -F: '{print $1}')"
 
-confFile="$(grep -r "\bserver_names_hash_max_size\b" /etc/nginx/nginx.conf /etc/nginx/conf.d/  | grep -vE "botblocker-nginx-settings.conf" | awk -F: '{print $1}' | head -n1)"
-sed 's/#server_names_hash_max_size/server_names_hash_max_size/g' -i $confFile
+while IFS= read -r confFile; do
+    if [ -f "$confFile" ]; then
+        sed 's/#server_names_hash_max_size/server_names_hash_max_size/g' -i $confFile
+    fi
+done <<<"$(grep -r "\bserver_names_hash_max_size\b" /etc/nginx/nginx.conf /etc/nginx/conf.d/  | grep -vE "botblocker-nginx-settings.conf" | awk -F: '{print $1}')"
