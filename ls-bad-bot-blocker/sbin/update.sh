9bf22a84d88329d0de9fab50f59340ea58dde95e
Raul Adorean <raul@layershift.me>
2025-04-23 09:32:06 +0000

    Run sed only if a file is found

--- ls-bad-bot-blocker/sbin/update.sh
+++ ls-bad-bot-blocker/sbin/update.sh
@@ -20,11 +20,15 @@ while IFS= read -r domain; do
 done <<<"$(plesk bin domain -l)"
 
 while IFS= read -r confFile; do
-    sed 's/server_names_hash_bucket_size/#server_names_hash_bucket_size/g' -i $confFile
+    if [ -f $confFile ]; then
+        sed 's/server_names_hash_bucket_size/#server_names_hash_bucket_size/g' -i $confFile
+    fi
 done <<<"$(grep -r "\bserver_names_hash_bucket_size\b" /etc/nginx/nginx.conf /etc/nginx/conf.d/  | grep -vE "botblocker-nginx-settings.conf|#" | awk -F: '{print $1}')"
 
 while IFS= read -r confFile; do
-    sed 's/server_names_hash_max_size/#server_names_hash_max_size/g' -i $confFile
+    if [ -f $confFile ]; then
+        sed 's/server_names_hash_max_size/#server_names_hash_max_size/g' -i $confFile
+    fi
 done <<<"$(grep -r "\bserver_names_hash_max_size\b" /etc/nginx/nginx.conf /etc/nginx/conf.d/  | grep -vE "botblocker-nginx-settings.conf|#" | awk -F: '{print $1}')"
 
 exit 0;
